189ccf7c292bcc7c25042c98d03a79e28be8f25f
Rohan Martin <136007645+rohanxd1@users.noreply.github.com>
2023-06-16 12:48:00 +0530

    rgb using pil into main

---
+++ Python/findrgb.py
@@ -0,0 +1,9 @@
+from PIL import Image
+img=Image.open(r"C:\Users\rohan\Downloads\Pictures\macro.jpeg")
+height=img.height
+width=img.width
+
+print(f"Height:{height}  Width:{width}")
+r,g,b=img.getpixel((100,100))
+print(f"R:{r} G:{g} B:{b}")
+img.show()
